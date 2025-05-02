0a002ee75d605f1733067a55b24f948aae246cca
Irina Rakhmankulova <arina2009.91@mail.ru>
2025-05-02 22:52:53 +0500

    init

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from src.widget import mask_account_card, get_date
 def main():
     card_example = "7000792289606361"
     account_example = "73654108430135874305"
-    type_and_number = 'visa platinum 7000792289606361'
+    type_and_number = "visa platinum 7000792289606361"
     date = "2024-03-11T02:26:18.671407"
     masked_card = get_mask_card_number(card_example)
     masked_account = get_mask_account(account_example)
@@ -16,6 +16,6 @@ def main():
     print(mask_account_or_card)
     print(masked_date)
 
+
 if __name__ == "__main__":
     main()
-
--- src/widget.py
+++ src/widget.py
@@ -1,37 +1,36 @@
 from src.masks import get_mask_account, get_mask_card_number
-def mask_account_card(type_and_number: str) -> str:
-    '''
-    функция содержит информацию о типе и номере карты или счета
-    '''
 
+
+def mask_account_card(type_and_number: str) -> str:
+    """
+    функция содержит информацию о типе и номере карты или счета
+    """
     lower_type_and_number = type_and_number.lower()
-    number = ''
+    number = ""
     for char in lower_type_and_number:  # отдельно записываем номер карты или счета
         if char.isdigit():
             number = number + char
-
-    if 'счет' in  lower_type_and_number:  # проверяем тип данных и маскируем номер счета или карты
+    if "счет" in lower_type_and_number:  # проверяем тип данных и маскируем номер счета или карты
         account_example = number
         masked_account = get_mask_account(account_example)
-        new_tupe_and_number = f'Счет: {masked_account}'
-    elif 'visa platinum' in lower_type_and_number:
+        new_tupe_and_number = f"Счет: {masked_account}"
+    elif "visa platinum" in lower_type_and_number:
         card_example = number
         masked_card = get_mask_card_number(card_example)
-        new_tupe_and_number = f'Visa Platinun: {masked_card}'
-    elif 'maestro' in lower_type_and_number:
+        new_tupe_and_number = f"Visa Platinun: {masked_card}"
+    elif "maestro" in lower_type_and_number:
         card_example = number
         masked_card = get_mask_card_number(card_example)
-        new_tupe_and_number = f'Maestro: {masked_card}'
+        new_tupe_and_number = f"Maestro: {masked_card}"
     return new_tupe_and_number
 
 
 def get_date(date: str) -> str:
+    """
+    Функция для форматирования даты
+    """
     day = date[8:10]
     month = date[5:7]
     year = date[0:4]
-    new_date = f'{day}.{month}.{year}'
+    new_date = f"{day}.{month}.{year}"
     return new_date
-
-
-
-
